3b0136fff022ec382ade079aec44c8bcc443d389
Zinan100 <97530806+Zinan100@users.noreply.github.com>
2022-01-24 11:44:18 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -136,15 +136,17 @@ def start(bot: Bot, update: Update, args: List[str]):
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
 
-                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([InlineKeyboardButton('➕️  Add me to your group ➕️', url="t.me/{}?startgroup=true".format(bot.username))
-            ],[
+                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup((
+            (InlineKeyboardButton('➕️  Add me to your group ➕️', url="t.me/{}?startgroup=true".format(bot.username))
+            ),(
             InlineKeyboardButton('🔊Channel🔊', url='https://t.me/moviespot00100'),
             InlineKeyboardButton('💬Group💬', url='https://t.me/moviespot001100')
-            ],[
+            ),(
             InlineKeyboardButton('🛠Help🛠', callback_data='help')
-            ],[
+            ),(
             InlineKeyboardButton('🧑‍💻Dev🧑‍💻', url='https://t.me/Zinan00100')
-            ])
+            ))
+            
     else:
         update.effective_message.reply_text("ചത്തിട്ടില്ലാ...")
 
